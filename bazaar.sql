929276af61e07dd6a58963791da7f6be385f1367
Chouffe <chouffe.caillau@gmail.com>
2018-09-25 18:31:57 +0200

    [db] switch to Postgres
    * Add UUID in entities
    * Add proper date for entities

--- bazaar.sql
+++ bazaar.sql
@@ -1,19 +1,24 @@
 BEGIN TRANSACTION;
 
+-- Enable UUID plugin
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 -- Tables Creations
 CREATE TABLE IF NOT EXISTS "item" (
-  "id"          INTEGER PRIMARY KEY,
-  "title"       VARCHAR NOT NULL,
-  "description" VARCHAR NULL,
-  "url"         VARCHAR NOT NULL,
-  "item_type"   VARCHAR NOT NULL,
-  "created_at"  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  "id"          INTEGER   PRIMARY KEY,
+  "title"       VARCHAR   NOT NULL,
+  "description" VARCHAR   NULL,
+  "url"         VARCHAR   NOT NULL,
+  "item_type"   VARCHAR   NOT NULL,
+  "created_at"  DATE      NULL      DEFAULT NULL,
+  "uuid"        UUID      NOT NULL  DEFAULT UUID_GENERATE_V4()
 );
 
 CREATE TABLE IF NOT EXISTS "author" (
   "id"         INTEGER PRIMARY KEY,
   "first_name" VARCHAR NOT NULL,
-  "last_name"  VARCHAR NOT NULL
+  "last_name"  VARCHAR NOT NULL,
+  "uuid"       UUID    NOT NULL DEFAULT UUID_GENERATE_V4()
 );
 
 CREATE TABLE IF NOT EXISTS "item_author" (
@@ -37,8 +42,8 @@ CREATE TABLE IF NOT EXISTS "item_tag" (
 
 -- Data Insertion
 INSERT INTO "item" (id, title, description, url, item_type, created_at) VALUES
-  (1, 'Escape from the ivory tower: the Haskell journey', 'In this talk Simon discusses Haskell’s birth and evolution, including some of the research and engineering challenges he faced in design and implementation.', 'https://www.youtube.com/watch?v=re96UgMk6GQ', 'Video', CURRENT_TIMESTAMP),
-  (2, 'Adventure with Types in Haskell', 'Recorded at Oregon Programming Languages Summer School 2013', 'https://www.youtube.com/watch?v=6COvD8oynmI', 'Video', CURRENT_TIMESTAMP);
+  (1, 'Escape from the ivory tower: the Haskell journey', 'In this talk Simon discusses Haskell’s birth and evolution, including some of the research and engineering challenges he faced in design and implementation.', 'https://www.youtube.com/watch?v=re96UgMk6GQ', 'Video', '2017-03-01'),
+  (2, 'Adventure with Types in Haskell', 'Recorded at Oregon Programming Languages Summer School 2013', 'https://www.youtube.com/watch?v=6COvD8oynmI', 'Video', '2013-07-01');
 
 INSERT INTO "tag" (id, name) VALUES
   (1, 'type class'),
