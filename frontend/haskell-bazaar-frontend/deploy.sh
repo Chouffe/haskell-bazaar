d03696cef9e1b1a255ac261e4731a7848d666c4c
Chouffe <chouffe.caillau@gmail.com>
2018-10-11 12:12:55 +0200

    [frontend] change deploy script

--- frontend/haskell-bazaar-frontend/deploy.sh
+++ frontend/haskell-bazaar-frontend/deploy.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+GIT_SHA=`git rev-parse HEAD`
+
 # Compiling Clojurescript -> JS
+echo "Building artifacts"
 lein clean
 lein cljsbuild once prod
 
+echo "Saving artifacts"
+# TODO: package the build with Branch and GIT_SHA in a S3 Bucket
+
 # Syncing to S3 with public read access
+echo "Deploying"
 aws s3 \
   sync resources/public/ s3://haskell-bazaar/ \
   --exclude 'js/compiled/out/*' \
