c27e6e3870d7aeeeeadf2bc2cf07d8ade2d9e866
Chouffe <chouffe.caillau@gmail.com>
2018-10-11 12:39:07 +0200

    [frontend] add gitignore and improve deploy script

--- frontend/haskell-bazaar-frontend/deploy.sh
+++ frontend/haskell-bazaar-frontend/deploy.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
+# Config
 BRANCH=`git rev-parse --abbrev-ref HEAD`
 GIT_SHA=`git rev-parse HEAD`
+BUILD_DIR=build/
+ARTIFACT_NAME=frontend.$BRANCH.$GIT_SHA.tar.gz
+S3_BUCKET=haskell-bazaar-builds/frontend/
+
+mkdir -p $BUILD_DIR
 
 # Compiling Clojurescript -> JS
 echo "Building artifacts"
 lein clean
 lein cljsbuild once prod
 
-echo "Saving artifacts"
-# TODO: package the build with Branch and GIT_SHA in a S3 Bucket
+echo "Compressing artifacts"
+tar \
+  --exclude 'js/compiled/out/*/' \
+  -cvzf $BUILD_DIR/$ARTIFACT_NAME \
+  resources/public/
+
+# Saving artifacts to S3
+echo "Saving Artifacts"
+aws s3 cp \
+  $BUILD_DIR/$ARTIFACT_NAME \
+  s3://$S3_BUCKET
+
+echo "Cleaning up local artifacts"
+rm $BUILD_DIR/$ARTIFACT_NAME
 
 # Syncing to S3 with public read access
 echo "Deploying"
